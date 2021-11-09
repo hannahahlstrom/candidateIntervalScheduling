58e62cc78a009db4abf2c34f10f8736bb15502bb
Brandon Hall <hall.brandon6374@gmail.com>
2021-11-09 10:25:32 -0500

    First edits to assignment.java

---
+++ Assignment.java
@@ -0,0 +1,12 @@
+import java.time.LocalDateTime
+
+public class Assignment {
+  private String title;
+  private String type;
+  private int class;
+  private double weight;
+  private LocalDateTime dueDate;
+
+
+
+}
